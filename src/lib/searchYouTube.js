d0dad68c78191da057f70c03ccbbee6d56ee75da
Chelsea Schmidt <cschmidt331@gmail.com>
2020-05-27 17:19:30 -0700

    Started work on searchYouTube

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,56 @@
+import YOUTUBE_API_KEY from '../config/youtube.js';
+
 var searchYouTube = (options, callback) => {
-  // TODO
+  //Use jQuery to send a GET request to the search endpoint. This is the only time you should use jQuery in this sprint
+  // setup the $.ajax({
+    // url,
+    // type,
+    // data { },
+    // content,
+    // success,
+    // error
+  // });
+
+  //Accept a callback function that is invoked with the videos array that is returned from hitting the endpoint
+
+  //Accept an options object with the following properties: query (the search string), max (the maximum number of videos to get, default to 5), key (API key)
+  //
+
+  //Only GET embeddable videos
+
 };
 
 export default searchYouTube;
+
+//https://www.googleapis.com/youtube/v3/search
+//Returns a collection of search results that match the query parameters
+//specified in the API request. By default, a search result set
+//identifies matching video, channel, and playlist resources, but you can
+//also configure queries to only retrieve a specific type of resource.
+
+//If successful, this method returns a response body with the following structure:
+
+// {
+//   "kind": "youtube#searchListResponse",
+//   "etag": etag,
+//   "nextPageToken": string,
+//   "prevPageToken": string,
+//   "regionCode": string,
+//   "pageInfo": {
+//     "totalResults": integer,
+//     "resultsPerPage": integer
+//   },
+//   "items": [
+//     search Resource
+//   ]
+// }
+
+//Youtube provided example:
+// function searchByKeyword() {
+//   var results = YouTube.Search.list('id,snippet', {q: 'dogs', maxResults: 25});
+
+//   for(var i in results.items) {
+//     var item = results.items[i];
+//     Logger.log('[%s] Title: %s', item.id.videoId, item.snippet.title);
+//   }
+// }
